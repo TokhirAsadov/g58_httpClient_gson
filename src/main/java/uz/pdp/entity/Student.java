f390253c112c72f39ad0bd8aed9d5ae87d68afe2
Takhir <guvalakat1603@gmail.com>
2025-10-21 09:57:14 +0500

    Student entity

---
+++ src/main/java/uz/pdp/entity/Student.java
@@ -0,0 +1,18 @@
+package uz.pdp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    private Long id;
+    private String name;
+    private Integer age;
+    private Double gpa;
+    private Group group;
+}
